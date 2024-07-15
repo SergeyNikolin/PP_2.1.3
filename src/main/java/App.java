6c98da8100c3311be9586229846cbbf516c38505
Nikolin Sergey <nikolinsergei2000@gmail.com>
2024-07-15 20:08:32 +0300

    Задание 2.1.3 выполнено

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hw1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld hw2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat c1 = (Cat) applicationContext.getBean("cat");
+        Cat c2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("hw1 == hw2: " + (hw1 == hw2));
+        System.out.println("c1 == c2: " + (c1 == c2));
     }
 }
